28e66c405d3f3819582aaeb720ae17b0adc3cf9e
snitovlad <snitovov@gmail.com>
2023-12-19 14:17:39 +0300

    German flag

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,1 +1,8 @@
-// code
+const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
+
+function createColorString() {
+    return colors.filter(item => item === "черный" || item === "красный" || item === "желтый").join('-')
+}
+
+console.log(createColorString());
+
